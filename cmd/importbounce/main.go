0917ce8bf7c3a7ff61bb777bad01cd09355c65cf
Alex Hamlin <alex@alexhamlin.co>
2019-10-09 14:53:23 -0700

    Implement config retrieval
    This way we can get a changeable configuration out of S3 or something,
    so you don't have to re-deploy the Lambda function every time you have a
    new package.

--- cmd/importbounce/main.go
+++ cmd/importbounce/main.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
+	"github.com/BurntSushi/toml"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
+	"golang.org/x/xerrors"
 )
 
 func main() {
+	http.DefaultClient.Timeout = 10 * time.Second
+
 	httpAddr := flag.String("http", "", "Serve HTTP on the provided address instead of AWS Lambda")
 	flag.Parse()
 
@@ -41,4 +47,38 @@ type bouncer struct {
 }
 
 func (b *bouncer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	config, err := b.loadConfig()
+	if err != nil {
+		log.Printf("failed to load config: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// TODO: not this
+	json.NewEncoder(w).Encode(config)
+}
+
+type config struct {
+	Packages []packageConfig `toml:"packages"`
+}
+
+type packageConfig struct {
+	Prefix   string `toml:"prefix"`
+	Import   string `toml:"import"`
+	Redirect string `toml:"redirect"`
+}
+
+func (b *bouncer) loadConfig() (config, error) {
+	resp, err := http.Get(b.ConfigURL)
+	if err != nil {
+		return config{}, xerrors.Errorf("fetching config: %w", err)
+	}
+	defer resp.Body.Close()
+
+	var c config
+	_, err = toml.DecodeReader(resp.Body, &c)
+	if err != nil {
+		err = xerrors.Errorf("decoding config: %w", err)
+	}
+	return c, err
 }
